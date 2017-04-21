2106ceff9a50c20d52c74489ac7d1fba38f862d0
Adam Bottchen <adam.bottchen@puppet.com>
2017-04-21 09:17:04 -0700

    Check to make sure the pe_server_version fact is available before using it -update

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 class codemgmt_1055_hotfix {
-  if($::pe_server_version != "") {
-    if ((versioncmp('2017.2.0', $::pe_server_version) > 0) and (versioncmp('2016.5.0', $::pe_server_version) < 0)){
+  if(has_key($facts, 'pe_server_version')) {
+    if ((versioncmp('2017.2.0', $facts['pe_server_version']) > 0) and (versioncmp('2016.5.0', $facts['pe_server_version']) < 0)){
       file {'/opt/puppetlabs/server/apps/puppetserver/bin/generate-puppet-types.rb':
         ensure => file,
         source => 'puppet:///modules/codemgmt_1055_hotfix/generate-puppet-types.rb',
